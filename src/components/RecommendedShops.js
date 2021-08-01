4651d55bcd1559a8ad30aa7e733c1a4211d85166
Abdulaziz930 <abdulazizak@code.edu.az>
2021-08-01 16:29:03 +0400

    update recommended shops slider

--- src/components/RecommendedShops.js
+++ src/components/RecommendedShops.js
@@ -20,6 +20,32 @@ const RecomendedShops = () => {
     autoplay: true,
     autoplaySpeed: 2000,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+    ],
   };
   return (
     <div className='recomendedShops'>
