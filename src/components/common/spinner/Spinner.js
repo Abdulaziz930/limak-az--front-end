6f3b3cae44d6f9fed3eecaf36c65a3ca0be0bc58
Abdulaziz930 <abdulazizak@code.edu.az>
2021-08-31 00:11:39 +0400

    update spinner style

--- src/components/common/spinner/Spinner.js
+++ src/components/common/spinner/Spinner.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className='d-flex justify-content-center spinner-box'>
-      <div className='spinner-grow text-primary-color' role='status'>
-        <span className='sr-only'>Loading...</span>
+    <div className='spinner'>
+      <div className='d-flex justify-content-center spinner-box'>
+        <div className='spinner-grow text-primary-color' role='status'>
+          <span className='sr-only'>Loading...</span>
+        </div>
       </div>
     </div>
   );
